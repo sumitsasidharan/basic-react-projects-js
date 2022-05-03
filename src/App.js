84189b8dc03caee7ce672586f711e63fd58fcc3e
sumit-onorall <sumit-onorall@gmail.com>
2022-05-03 20:53:37 +0530

    updated food menu project

--- src/App.js
+++ src/App.js
@@ -1,7 +1,4 @@
 import MenuApp from './05-food-menu/MenuApp'
-import ToursMain from './02-tours/ToursMain';
-import ReviewApp from './03-reviews/ReviewApp';
-import AccordionApp from './04-accordion/AccordionApp';
 
 function App() {
   return (
@@ -12,3 +9,4 @@ function App() {
 }
 
 export default App;
+// 1.27.32

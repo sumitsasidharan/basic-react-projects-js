9b0072378adde635b4387d54d9a9385d60c45dfc
sumit-onorall <sumit-onorall@gmail.com>
2022-05-03 17:47:23 +0530

    created food menu project

---
+++ src/05-food-menu/MenuApp.jsx
@@ -0,0 +1,12 @@
+import React, { useState } from 'react';
+import Menu from './Menu';
+import Categories from './Categories';
+import items from './data';
+
+import './MenuApp.css';
+
+function App() {
+   return <h2>menu project setup</h2>;
+}
+
+export default App;
--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import MenuApp from './05-food-menu/MenuApp'
 import ToursMain from './02-tours/ToursMain';
 import ReviewApp from './03-reviews/ReviewApp';
 import AccordionApp from './04-accordion/AccordionApp';
@@ -5,7 +6,7 @@ import AccordionApp from './04-accordion/AccordionApp';
 function App() {
   return (
     <div className="App">
-      <AccordionApp />
+      <MenuApp />
     </div>
   );
 }

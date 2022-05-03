10d4909c439cf1c2b17dff6d39ed7777806f7058
sumit-onorall <sumit-onorall@gmail.com>
2022-05-03 16:58:12 +0530

    created accordion component

---
+++ src/04-accordion/AccordionApp.jsx
@@ -0,0 +1,11 @@
+import React, { useState } from 'react';
+import data from './data';
+import SingleQuestion from './Question';
+
+import './AccordionApp.css';
+
+function AccordionApp() {
+   return <h2>accordion project setup</h2>;
+}
+
+export default AccordionApp;
--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import ToursMain from './02-tours/ToursMain';
 import ReviewApp from './03-reviews/ReviewApp';
+import AccordionApp from './04-accordion/AccordionApp';
 
 function App() {
   return (
     <div className="App">
-      <ReviewApp />
+      <AccordionApp />
     </div>
   );
 }
 
 export default App;
-// 50.17
+// 1.17.39
